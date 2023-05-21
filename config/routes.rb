7a14c8ee0f87839cf023ff1d7a978eedb1ae6ac3
Ivan Takarlikov <vtakarlikov@gmail.com>
2023-05-20 23:20:28 -0300

    Add auth to sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.application.credentials[Rails.env.to_sym][:sidekiqweb][:username])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.credentials[Rails.env.to_sym][:sidekiqweb][:password]))
+    end
+
   mount Sidekiq::Web => '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
