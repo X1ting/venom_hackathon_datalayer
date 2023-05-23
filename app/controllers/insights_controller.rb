90f2e2ed3af75b66507099c29e50b5e4bbe26a8b
Ivan Takarlikov <vtakarlikov@gmail.com>
2023-05-23 08:53:06 -0300

    Add account insights

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -3,20 +3,40 @@ class InsightsController < ApplicationController
     @transactions = Transaction.where(created_at: 1.day.ago..)
     @transaction_insights = [
       {
-        name: 'Venom All',
+        name: 'Venom',
         data: @transactions.venom.devnet.group_by_minute(:created_at, n: 5).count
       },
       {
-        name: 'Everscale All',
+        name: 'Everscale',
         data: @transactions.everscale.mainnet.group_by_minute(:created_at, n: 5).count
       },
       {
-        name: 'NFT All',
+        name: 'NFT',
         data: @transactions.nft.group_by_minute(:created_at, n: 5).count
       },
       {
-        name: 'Dex All',
-        data: @transactions.nft.group_by_minute(:created_at, n: 5).count
+        name: 'DEX',
+        data: @transactions.dex.group_by_minute(:created_at, n: 5).count
+      }
+    ]
+
+    @accounts = Account.where(created_at: 1.day.ago..)
+    @accounts_insights = [
+      {
+        name: 'Venom',
+        data: @accounts.venom.devnet.group_by_minute(:created_at, n: 5).count
+      },
+      {
+        name: 'Everscale',
+        data: @accounts.everscale.mainnet.group_by_minute(:created_at, n: 5).count
+      },
+      {
+        name: 'NFT',
+        data: @accounts.nft.group_by_minute(:created_at, n: 5).count
+      },
+      {
+        name: 'Wallets',
+        data: @accounts.wallet.group_by_minute(:created_at, n: 5).count
       }
     ]
   end
