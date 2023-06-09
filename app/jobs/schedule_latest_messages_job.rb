f4e00daa39663f01e956dc479fee4060837bb6b4
Ivan Takarlikov <vtakarlikov@gmail.com>
2023-06-09 17:29:19 -0300

    add Everscale messages decoding

--- app/jobs/schedule_latest_messages_job.rb
+++ app/jobs/schedule_latest_messages_job.rb
@@ -14,5 +14,17 @@ class ScheduleLatestMessagesJob < ApplicationJob
           "try_to_decode_all" => true
         })
       end
+
+    Clickhouse::Everscale::Mainnet::Message
+      .where('created_at >= ?', BUFFER_TIME.ago)
+      .pluck(:id)
+      .uniq
+      .compact_blank
+      .in_groups_of(50) do |batch|
+        Everscale::DecodeMessagesJob.perform_async({
+          "message_ids" => batch.compact,
+          "try_to_decode_all" => true
+        })
+      end
   end
 end
