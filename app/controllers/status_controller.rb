a525abe55a4f1d0ee957894032a0cf74e76bf6b8
Ivan Takarlikov <vtakarlikov@gmail.com>
2023-06-09 18:44:21 -0300

    Add OK respond to status messages

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -3,7 +3,7 @@ class StatusController < ApplicationController
   def transactions
     count = Transaction.where(time: 5.minutes.ago..).count
     if count > 0
-      head :ok
+      render plain: 'OK', status: :ok
     else
       head :not_found
     end
