31f9013740e9042c1b6c48211741b44ed8e29713
Ivan Takarlikov <vtakarlikov@gmail.com>
2023-06-12 14:46:20 -0300

    Use environemnt based redis and host

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,11 @@ require "sidekiq/throttled"
 Sidekiq::Throttled.setup!
 
 Sidekiq.configure_server do |config|
+  redis = Rails.env.production? ? Rails.application.credentials.redis.hostname : 'redis://localhost:6379/1'
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-  # config.server_middleware do |chain|
-  #   chain.add Sidekiq::Server::Middleware::CollectJobStats
-  # end
 end
+
 Sidekiq.configure_client do |config|
+  redis = Rails.env.production? ? Rails.application.credentials.redis.hostname : 'redis://localhost:6379/1'
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
