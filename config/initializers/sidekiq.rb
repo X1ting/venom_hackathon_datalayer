17f3d6823d57e2e59575d01d99fc3c8dd3825622
Ivan Takarlikov <vtakarlikov@gmail.com>
2023-06-12 15:35:44 -0300

    Update redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,10 +3,10 @@ Sidekiq::Throttled.setup!
 
 Sidekiq.configure_server do |config|
   redis = Rails.env.production? ? Rails.application.credentials.redis.hostname : 'redis://localhost:6379/1'
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: redis }
 end
 
 Sidekiq.configure_client do |config|
   redis = Rails.env.production? ? Rails.application.credentials.redis.hostname : 'redis://localhost:6379/1'
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: redis }
 end
