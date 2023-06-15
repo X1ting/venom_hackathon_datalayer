30ce322a801ce0ef1b6aa2b75d7e4e07195e55b8
Ivan Takarlikov <vtakarlikov@gmail.com>
2023-06-15 12:14:54 -0300

    Increase venom concurrency

--- app/jobs/venom/decode_messages_job.rb
+++ app/jobs/venom/decode_messages_job.rb
@@ -2,6 +2,6 @@ module Venom
   class DecodeMessagesJob < DecodeMessagesJobBase
     include BaseJobMixin
     sidekiq_options :queue => :decode_messages_venom
-    sidekiq_throttle(concurrency: { limit: 5 })
+    sidekiq_throttle(concurrency: { limit: 8 })
   end
 end
